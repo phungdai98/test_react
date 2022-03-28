22f1a64f965e7b85818c93fafa7922a17a299ed8
Phung Dai <daipv@rever.vn>
2022-03-28 15:11:04 +0700

    Updates

---
+++ precache-manifest.67ca1ec2b8fd6fc5ad203714331d8442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5266c43606681d74a76b0cf3ab287b9e",
+    "url": "/phungdai98/test_react/index.html"
+  },
+  {
+    "revision": "315214444f0a77367920",
+    "url": "/phungdai98/test_react/static/css/2.5d9257f0.chunk.css"
+  },
+  {
+    "revision": "973ac02239c61cba8438",
+    "url": "/phungdai98/test_react/static/css/main.acc16fec.chunk.css"
+  },
+  {
+    "revision": "315214444f0a77367920",
+    "url": "/phungdai98/test_react/static/js/2.80cc1d0a.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/phungdai98/test_react/static/js/2.80cc1d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973ac02239c61cba8438",
+    "url": "/phungdai98/test_react/static/js/main.19b8241d.chunk.js"
+  },
+  {
+    "revision": "1c9f37cb3cc51232f3dd",
+    "url": "/phungdai98/test_react/static/js/runtime-main.97402cb8.js"
+  }
+]);
